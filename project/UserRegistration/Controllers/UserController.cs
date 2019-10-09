e0ce0f18092cf8ba6cd710d3b6153e1c12fce458
yambaO <yamba.ouandaogo@hotmail.com>
2019-10-09 05:10:52 -0400

    User's registration and log in

---
+++ project/UserRegistration/Controllers/UserController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using UserRegistration.Models;
+namespace UserRegistration.Controllers
+{
+    public class UserController : Controller
+    {
+        // GET: User
+        public ActionResult AddOrEdit(int id = 0)
+        {
+            User user = new User();
+            return View(user);
+        }
+        [HttpPost]
+        public ActionResult AddOrEdit(User user)
+        {
+            using ( DBModels db = new DBModels ())
+            {
+                db.Users.Add(user);
+                db.SaveChanges();
+            }
+            ModelState.Clear();
+            ViewBag.SuccessMessage = "Registration Successful.";
+            return View("AddOrEdit", new User() );
+        }
+    }
+}
